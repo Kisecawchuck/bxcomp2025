88df66d237a2cb04975ed1a68f4b7757f1df5aee
Igor <iny5051@gmail.com>
2025-09-13 12:46:55 -0300

    Melhorando a cifra de Cesar

--- etapa0/UgaBuga/uga.c
+++ etapa0/UgaBuga/uga.c
@@ -4,7 +4,7 @@ int main() {
     int c;
     while ((c = getchar()) != EOF) {
         if (c >= 'a' && c <= 'z') {
-            char r = (c + 23) % 26 + 'a';
+            char r = ((c - 'a' - 10) % 26 + 26) % 26 + 'a';
             putchar(r);
         } else putchar(c);
     }
