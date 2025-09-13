6008439e7ed031f184171c1af3b916f7ab476d31
Kisecawchuck <105141160+Kisecawchuck@users.noreply.github.com>
2025-09-13 13:44:46 -0300

    Update uga.c

--- etapa0/UgaBuga/uga.c
+++ etapa0/UgaBuga/uga.c
@@ -4,7 +4,9 @@ int main() {
     int c;
     while ((c = getchar()) != EOF) {
         if (c >= 'a' && c <= 'z') {
-            char r = ((c - 'a' - 10) % 26 + 26) % 26 + 'a';
+            char r = ((c - 'a' - 10) % 26 + 26) % 26 + 'a'; // Impede módulos negativos e melhora a versão passada;
+                                                            // O primeiro módulo não é necessário neste caso,
+                                                            // mas seria importante se o descolamento fosse maior do que 26;
             putchar(r);
         } else putchar(c);
     }
